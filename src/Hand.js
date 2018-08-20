7d6e7fdb80ad011eb85a09b9f5ae66547531afb9
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-08-20 19:01:49 -0400

    added deckId to Hand component's state

--- src/Hand.js
+++ src/Hand.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
 class Hand extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      deckId: null
+    }
+  }
 
   fetchDeck = (event) => {
     event.preventDefault()
     const shuffleDeckAPI = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
-    return fetch(shuffleDeckAPI).then(resp => resp.json()).then(respJSON => document.getElementById("test").innerHTML = respJSON.success)
+    return fetch(shuffleDeckAPI).then(resp => resp.json()).then(respJSON => this.setState({deckId: respJSON.deck_id})
+      // document.getElementById("test").innerHTML = respJSON.deck_id)
+   )
   }
 
   dealCard = (event) => {
     event.preventDefault();
-    const drawTwoCards = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
-    return fetch(drawTwoCards).then(resp => resp.json()).then(respJSON => document.getElementsByClassName("competitor")[1].innerHTML = respJSON.cards[1].value)
+    console.log(this.state.deckId)
+    const drawTwoCards = `https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/?count=2`
+    return fetch(drawTwoCards).then(resp => resp.json()).then(respJSON =>
+    console.log(respJSON.cards)
+    // document.getElementById("dealer").innerHTML = respJSON.cards[0].value
+    // document.getElementById("player").innerHTML = respJSON.cards[1].value
+  )
   }
 
   render() {
@@ -20,11 +33,11 @@ class Hand extends React.Component {
             <div id="test">
             </div>
 
-            <div className="competitor">
+            <div id="dealer">
               <h1>DEALER</h1>
             </div>
 
-            <div className="competitor">
+            <div id="player">
               <h1>PLAYER</h1>
             </div>
 
