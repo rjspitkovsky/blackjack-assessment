5a1926990916852c280b519415f115a886ee3b07
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-08-20 18:42:50 -0400

    drawCards calls API for two new calls and stubs out value to player div

--- src/Hand.js
+++ src/Hand.js
@@ -4,9 +4,14 @@ class Hand extends React.Component {
 
   fetchDeck = (event) => {
     event.preventDefault()
-    let request = new XMLHttpRequest()
     const shuffleDeckAPI = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
     return fetch(shuffleDeckAPI).then(resp => resp.json()).then(respJSON => document.getElementById("test").innerHTML = respJSON.success)
+  }
+
+  dealCard = (event) => {
+    event.preventDefault();
+    const drawTwoCards = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
+    return fetch(drawTwoCards).then(resp => resp.json()).then(respJSON => document.getElementsByClassName("competitor")[1].innerHTML = respJSON.cards[1].value)
   }
 
   render() {
@@ -14,9 +19,21 @@ class Hand extends React.Component {
       <div>
             <div id="test">
             </div>
+
+            <div className="competitor">
+              <h1>DEALER</h1>
+            </div>
+
+            <div className="competitor">
+              <h1>PLAYER</h1>
+            </div>
+
+
         <h1>This is my hand component</h1>
 
         <button onClick={event => this.fetchDeck(event)}>Start game!</button>
+
+        <button onClick={event => this.dealCard(event)}>Deal a card to each player</button>
       </div>
     )
   }
