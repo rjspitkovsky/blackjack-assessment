e0acc25f65428c785f1dd9e81adc23f8822cd567
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-08-20 18:27:35 -0400

    fetchdeck click action returns shuffled deck and success value: true

--- src/Hand.js
+++ src/Hand.js
@@ -2,11 +2,24 @@ import React from 'react';
 
 class Hand extends React.Component {
 
+  fetchDeck = (event) => {
+    event.preventDefault()
+    let request = new XMLHttpRequest()
+    const shuffleDeckAPI = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+    return fetch(shuffleDeckAPI).then(resp => resp.json()).then(respJSON => document.getElementById("test").innerHTML = respJSON.success)
+  }
+
   render() {
     return (
-      <h1>This is my hand component</h1>
+      <div>
+            <div id="test">
+            </div>
+        <h1>This is my hand component</h1>
+
+        <button onClick={event => this.fetchDeck(event)}>Start game!</button>
+      </div>
     )
   }
 }
 
-export default Hand 
+export default Hand
